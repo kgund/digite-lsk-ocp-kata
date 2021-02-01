f5a3474c716656e90ed86ac9bdb64b9535637253
kgund <kgund@digite.com>
2021-02-01 23:03:51 +0530

    Passing tests for Plane

--- src/main/java/com/digite/kata/eg2/Plane.java
+++ src/main/java/com/digite/kata/eg2/Plane.java
@@ -2,5 +2,13 @@ package com.digite.kata.eg2;
 
 public class Plane extends Vehicle {
     // A plane can reverse engine gear while moving forward, no problem here
+
+    @Override
+    public void changeGear(Gear gear) {
+        if(Gear.P.equals(gear) || Gear.N.equals(gear)){
+            throw new RuntimeException("Gear " + gear.toString() +" not available ");
+        }
+        super.changeGear(gear);
+    }
 }
 
